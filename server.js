1847043b3f2f19236bfd6a31f8dde1ddfdf86454
ChefBoyRRD <chris050503@gmail.com>
2025-11-03 14:10:59 -0500

    Routing

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const app = express(); //calling this function sets up a server
 
+const userRouter = require('./routes/users');
+
+const app = express(); //calling this function sets up a server
+app.set('view engine', 'ejs')
+app.use('/users', userRouter);
 
 app.get('/' , (req, res)=> {
 console.log('Here');
-res.send("Hello World!");
+res.render("Index", {user:"Chris!"});
 
-}); //this function will run when someone goes to the root function
+
+});
+app.get('/users/new', (req,res) =>{
+    res.send('User List');
+}) 
+app.get('/users/new',(req, res) =>{
+    res.send('New User Form');
+})
+//this function will run when someone goes to the root function
 app.listen(3030);
