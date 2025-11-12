ac99dc4ba335b0cce430ed3205832886ffeea95c
ChefBoyRRD <chris050503@gmail.com>
2025-11-11 21:07:27 -0500

    Routing

--- routes/users.js
+++ routes/users.js
@@ -10,5 +10,10 @@ res.send('User List');
 router.get('/new',(req,res)=>{
     res.send('New User Form');
 });
-
+router.get('/posts',(req,res)=>{
+    res.send('Post new');
+})
+router.get('/location',(req,res)=>{
+    res.send('Post location please');
+})
 module.exports = router;
