d67951ce179e46dd223128b0cf70b8cd71b2a88c
ChefBoyRRD <chris050503@gmail.com>
2025-12-06 04:13:35 -0500

    Final Part of the Project

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,60 @@
-const express = 
-require('express');
+const express = require("express");
+const router = express.Router();
 
-const router =
-express.Router();
+router.get("/", (req, res) => {
+  res.render(`users/list`, { users: users });
+});
 
-router.get('/', (req,res)=>{
-res.send('User List');
+router.get("/new", (req, res) => {
+  res.render("users/new", { firstName: "Please nter your name" });
 });
-router.get('/new',(req,res)=>{
-    res.send('New User Form');
+
+router.post("/", (req, res) => {
+  // res.send('Creating a new user');
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const age = req.body.age;
+  const gender = req.body.gender;
+
+  const isValid = firstName !== "" && lastName;
+  if (isValid) {
+    console.log(`Adding User: ${firstName}`);
+    users.push({
+      firstName: firstName,
+      lastName: lastName,
+      age: age,
+      gender: gender,
+    });
+
+    console.log(`New Set of Users: ${users}`);
+    res.send("Success!");
+
+  } else {
+    console.log("Invalid User Data");
+    res.render("users/new", { firstName: firstName });
+  }
 });
-router.get('/posts',(req,res)=>{
-    res.send('Post new');
-})
-router.get('/location',(req,res)=>{
-    res.send('Post location please');
-})
+
+// router.get('/:ide', (req, res) => {
+//     res.send(`Getting User data: ${req.params.id}`);
+// });
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    res.send(`Getting User Data: ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Deleting User data: ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Updating User data: ${req.params.id}`);
+  });
+
+const users = [];
+
+router.param("id", (req, res, next, id) => {
+  console.log(`Accessing user: #${id}`);
+  next();
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 const express = require('express');
-
 const userRouter = require('./routes/users');
+const postRouter = require('./routes/posts');
+const app = express(); //Calling this function sets up a server
 
-const app = express(); //calling this function sets up a server
-app.set('view engine', 'ejs')
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+// app.use(logger);
+
+
 app.use('/users', userRouter);
-
-app.get('/' , (req, res)=> {
-console.log('Here');
-res.render("Index", {user:"Chris!"});
+app.use('/postsLocation', postRouter);
 
 
+app.get('/', (req, res) => {
+    console.log('Someone made a request!');
+    res.render("index", {user: "Chris!"});
 });
-app.get('/users/new', (req,res) =>{
-    res.send('User List');
-}) 
-app.get('/users/new',(req, res) =>{
-    res.send('New User Form');
+app.get('/new', (req,res)=>{
+    res.render('users/new')
 })
-//this function will run when someone goes to the root function
+
+
 app.listen(3030);
+
+function logger(req, res, next) {
+    console.log(`Page Accessed: ${req.originalUrl}`);
+    next();
+}   
